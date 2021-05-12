dd3996ab891b2d648e7ea1cafc8bd3c3a793cbca
david <cingolanidavid@gmail.com>
2021-05-12 13:02:22 -0300

    add keyframes

--- src/components/TextIn/TextIn.js
+++ src/components/TextIn/TextIn.js
@@ -15,12 +15,30 @@ export const TextIn = () => {
         initial={[
           {
             targets: "#Box",
-            translateX: 300,
-            easing: "linear",
+            keyframes: [
+              {
+                translateX: 150,
+              },
+              {
+                translateY: 150,
+              },
+              {
+                translateX: 0,
+              },
+              {
+                translateY: 0,
+              },
+            ],
+            // easing:'spring',
+            duration: 3500,
+            loop: true,
           },
         ]}
       >
-        <h1 id="Box" style={{ height: 50, width: 50, color: "#00000" }}> React AnimeJS Package </h1>
+        <h1 id="Box" style={{ height: 50, width: 50, color: "#00000" }}>
+          {" "}
+          React AnimeJS Package{" "}
+        </h1>
       </Anime>
     </TextInContainer>
   );
